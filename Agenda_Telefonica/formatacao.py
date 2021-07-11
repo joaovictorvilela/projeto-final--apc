4f29d951f2d88b01048481758eb239453b2d84ee
João Victor Vilela dos Santos <74119884+joaovictorvilela@users.noreply.github.com>
2021-07-11 17:50:14 -0300

    Update formatacao.py

--- Agenda_Telefonica/formatacao.py
+++ Agenda_Telefonica/formatacao.py
@@ -1,8 +1,6 @@
-# importando o módulo sleep do python para dar um efeito na função sair
 from time import sleep
 
 
-# função que irá exibir as opções disponíveis para o usuário
 def menu():
     print('-=' * 30) 
     print(f'{"Menu de Opções":^60}')
@@ -10,9 +8,7 @@ def menu():
     print('[1] - Adicionar Contato\n[2] - Remover Contato\n[3] - Alterar Contato\n[4] - Listar Contatos\n[5] - Localizar Contato\n[0] - Sair')
     print('-=' * 30)
 
-# função que irá exibir uma mensagem para o usuário quando ele escolher sair
 def sair():
     print('Encerrando o programa...')
-    # aqui entra o sleep ele irá esperar um tempo para mostrar o próximo print
     sleep(1)
     print('Obrigado e volte sempre!')
